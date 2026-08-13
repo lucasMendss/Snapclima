ec9c75197c1046d98a01d0872396c0a4af60e7a1
Lucas Mendes <lucasrafaelmendes8@gmail.com>
2026-08-12 21:35:46 -0300

    Add weather API handler and update references in app.js; modify .gitignore and index.html

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const sunriseTime = document.getElementById('sunrise-time');
 const sunsetTime = document.getElementById('sunset-time');
 
 // backend vars
-const WEATHER_API_URL = "https://snapclima-one.vercel.app/api/weather";
+const WEATHER_API_URL = "https://snapclima-one.vercel.app/api/v1/weather";
 let isCelsiusUnitActive = true;
 
 requestCurrentLocationWeather();
